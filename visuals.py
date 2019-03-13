5c8a4217210907e2b2826d7164afd0cf4c382bbe
sauter-roland <sauter.roland@googlemail.com>
2019-03-13 15:25:04 +0100

    Adding docstrings for visuals

--- visuals.py
+++ visuals.py
@@ -3,7 +3,13 @@ from random_surface import read_np_random_surface
 from bike import Bike
 
 
-def draw(floor, loc_bike):
+def _draw(floor, loc_bike):
+	"""
+	Draws current position of the bike along with the floor.
+	:param floor: Numpy array of floor (0-th column is x-axis)
+	:param loc_bike: Numpy array containing current location of bike
+	:return: No return value
+	"""
 	LENGTH_STAGE = 100
 	HEIGHT_STAGE = 10
 	SIZE_WHEEL = .2
@@ -16,18 +22,28 @@ def draw(floor, loc_bike):
 	ax.set_xlim(0, LENGTH_STAGE)
 	ax.set_ylim(0, HEIGHT_STAGE)
 	
-	fig_floor = draw_floor(floor)
-	fig_bike = draw_bike(loc_bike)
+	fig_floor = _draw_floor(floor)
+	fig_bike = _draw_bike(loc_bike)
 	
 	pp.draw()
 
 
-def draw_floor(floor):
+def _draw_floor(floor):
+	"""
+	Draws the floor.
+	:param floor: Floor as in draw function.
+	:return: Figure object
+	"""
 	fig = pp.plot(floor[0, :], floor[1, :], color='g')
 	return fig
 
 
-def draw_bike(loc_bike):
+def _draw_bike(loc_bike):
+	"""
+	Draws the bike.
+	:param loc_bike: Bike as in draw function.
+	:return: Figure object
+	"""
 	fig = pp.plot(loc_bike[0, :2], loc_bike[1, :2], 'bo', loc_bike[0, 2:], loc_bike[1, 2:], 'r^', markersize=5)
 	for i in range(4):
 		for j in range(i, 4):
@@ -36,8 +52,14 @@ def draw_bike(loc_bike):
 
 
 def animate(floor, locs_bike):
+	"""
+	Iteratively calls draw and pauses on all the bike locations.
+	:param floor: Numpy array of floor (0-th column is x-axis)
+	:param locs_bike: List of Numpy arrays containing current location of bike.
+	:return: Nothing
+	"""
 	for loc in locs_bike:
-		draw(floor, loc)
+		_draw(floor, loc)
 		pp.pause(.5)
 
 
